53efffa5ae92d8e4b568ccd2f7d31a39e34ee11a
tankhellfire <114331841+tankhellfire@users.noreply.github.com>
2024-04-02 11:26:05 +0000

    i dont know

--- test.cpp
+++ test.cpp
@@ -5,36 +5,12 @@
 
 int main(int argc, char* argv[])
 {
-    HANDLE file = CreateFile("C:/Users/tt/Downloads/m.txt", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL, NULL);
-    WriteFile(file, L"a\n",sizeof(wchar_t)*3, NULL, NULL);
-    HANDLE shm_handle  = CreateFileMappingW(
-    INVALID_HANDLE_VALUE,
-    NULL,
-    PAGE_READWRITE,
-    0,
-    4,
-    L"main.exe");
-    if (shm_handle  == NULL) {
-        WriteFile(file, L"0\n", sizeof(wchar_t)*3, NULL, NULL);
-        return 1;
-    }
+    HANDLE shm_handle  = CreateFileMappingW(INVALID_HANDLE_VALUE,NULL,PAGE_READWRITE,0,4,L"main.exe");
 
-    void* shm_ptr = MapViewOfFile(
-        shm_handle,
-        FILE_MAP_ALL_ACCESS,
-        0,
-        0,
-        4);
-
-    if (shm_ptr == NULL) {
-        WriteFile(file, L"1\n",sizeof(wchar_t)*3, NULL, NULL);
-        CloseHandle(shm_handle);
-        return 1;
-    }
+    void* shm_ptr = MapViewOfFile(shm_handle,FILE_MAP_ALL_ACCESS,0,0,4);
     char* shm_data = static_cast<char*>(shm_ptr);
-
     
-    WriteFile(file, shm_data, strlen(shm_data), NULL, NULL);
 
-    strcpy_s(shm_data, 4, 0x00000000);
+    *reinterpret_cast<uint32_t*>(shm_data) = 0x11121314;
+    while(true){};
 }
