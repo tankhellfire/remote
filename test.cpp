2779814f3cbf405c4f6cd1eb815ec76293b62a06
tankhellfire <114331841+tankhellfire@users.noreply.github.com>
2024-04-12 04:50:37 +0000

    gg ezy

--- test.cpp
+++ test.cpp
@@ -10,7 +10,13 @@ int main(int argc, char* argv[])
     void* shm_ptr = MapViewOfFile(shm_handle,FILE_MAP_ALL_ACCESS,0,0,4);
     char* shm_data = static_cast<char*>(shm_ptr);
     
+    *reinterpret_cast<uint32_t*>(shm_data) = GetTickCount();
 
-    *reinterpret_cast<uint32_t*>(shm_data) = 0x11121314;
-    while(true){};
+    HANDLE file = CreateFile("C:/Users/tt/Downloads/m.txt", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL, NULL);
+
+    std::string utf8_str = std::to_string(*shm_data) + std::string(" , ") + std::to_string(GetTickCount());
+    
+    WriteFile(file, utf8_str.c_str(), static_cast<DWORD>(utf8_str.length()), NULL, NULL);
+
+    while(GetTickCount()-*shm_data<20000){};
 }
