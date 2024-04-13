83dbc9022fd16c442a021a206fc94260929771fa
tankhellfire <114331841+tankhellfire@users.noreply.github.com>
2024-04-13 05:49:43 +0000

    less go

--- test.cpp
+++ test.cpp
@@ -5,18 +5,17 @@
 
 int main(int argc, char* argv[])
 {
-    HANDLE shm_handle  = CreateFileMappingW(INVALID_HANDLE_VALUE,NULL,PAGE_READWRITE,0,4,L"main.exe");
+    HANDLE shm_handle  = CreateFileMappingW(INVALID_HANDLE_VALUE,NULL,PAGE_READWRITE,0,8,L"JrRKULBcTr");
+    void* shm_ptr = MapViewOfFile(shm_handle,FILE_MAP_ALL_ACCESS,0,0,8);
 
-    void* shm_ptr = MapViewOfFile(shm_handle,FILE_MAP_ALL_ACCESS,0,0,4);
-    char* shm_data = static_cast<char*>(shm_ptr);
+    uint32_t* a = static_cast<uint32_t*>(shm_ptr);
+    uint32_t* shm_data = reinterpret_cast<uint32_t*>(static_cast<uint8_t*>(shm_ptr) + 4);
     
-    *reinterpret_cast<uint32_t*>(shm_data) = GetTickCount();
-
-    HANDLE file = CreateFile("C:/Users/tt/Downloads/m.txt", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL, NULL);
-
-    std::string utf8_str = std::to_string(*shm_data) + std::string(" , ") + std::to_string(GetTickCount());
+    *static_cast<uint32_t*>(shm_data) = GetTickCount();
     
-    WriteFile(file, utf8_str.c_str(), static_cast<DWORD>(utf8_str.length()), NULL, NULL);
 
-    while(GetTickCount()-*shm_data<20000){};
+    while(GetTickCount()-(static_cast<uint32_t>(*shm_data))<2000){
+        Sleep(100);
+        *static_cast<uint32_t*>(shm_data) = GetTickCount();
+    };
 }
