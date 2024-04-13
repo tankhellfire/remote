20a5a054372e97523ad7b9ee3c532bd1f43fa5cb
tankhellfire <114331841+tankhellfire@users.noreply.github.com>
2024-04-13 08:57:32 +0000

    why

--- test.cpp
+++ test.cpp
@@ -20,15 +20,13 @@ int main(int argc, char* argv[])
     char path[MAX_PATH]="";
     GetModuleFileName(NULL, path, MAX_PATH);
 
-    HANDLE file = CreateFile("C:/Users/tt/Downloads/m.txt", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL, NULL);
-    std::string meStr = std::to_string(me);
-    WriteFile(file, meStr.c_str(), strlen(meStr.c_str()), NULL, NULL);
+    std::string meStr;
 
     //while(GetTickCount()-(static_cast<uint32_t>(*myMem))<2000){
     while(true){
         *static_cast<uint32_t*>(myMem) = GetTickCount();
-        WriteFile(file, std::to_string(static_cast<uint32_t>(*theirMem)).c_str(), strlen(std::to_string(static_cast<uint32_t>(*theirMem)).c_str()), NULL, NULL);
-        if(200<GetTickCount()-(static_cast<uint32_t>(*theirMem))){
+        if(200<(GetTickCount()-(static_cast<uint32_t>(*theirMem)))){
+
             meStr=std::string(path)+std::string(" ")+std::to_string((me+1)%2);
             ShellExecute(NULL, "open",meStr.c_str(), NULL, NULL, SW_SHOWNORMAL);
         }
