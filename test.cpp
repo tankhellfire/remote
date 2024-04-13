203d3e2749dc04b01542d5383646566d84030068
tankhellfire <114331841+tankhellfire@users.noreply.github.com>
2024-04-13 06:30:19 +0000

    this is that

--- test.cpp
+++ test.cpp
@@ -8,14 +8,18 @@ int main(int argc, char* argv[])
     HANDLE shm_handle  = CreateFileMappingW(INVALID_HANDLE_VALUE,NULL,PAGE_READWRITE,0,8,L"JrRKULBcTr");
     void* shm_ptr = MapViewOfFile(shm_handle,FILE_MAP_ALL_ACCESS,0,0,8);
 
-    uint32_t* a = static_cast<uint32_t*>(shm_ptr);
-    uint32_t* shm_data = reinterpret_cast<uint32_t*>(static_cast<uint8_t*>(shm_ptr) + 4);
-    
-    *static_cast<uint32_t*>(shm_data) = GetTickCount();
-    
+    uint32_t* myMem = static_cast<uint32_t*>(shm_ptr);
+    uint32_t* theirMem = reinterpret_cast<uint32_t*>(static_cast<uint8_t*>(shm_ptr) + 4);
 
-    while(GetTickCount()-(static_cast<uint32_t>(*shm_data))<2000){
+    char path[MAX_PATH]="";
+    GetModuleFileName(NULL, path, MAX_PATH);
+
+    //while(GetTickCount()-(static_cast<uint32_t>(*myMem))<2000){
+    while(true){
+        *static_cast<uint32_t*>(myMem) = GetTickCount();
+        if(200<GetTickCount()-(static_cast<uint32_t>(*theirMem))){
+            ShellExecute(NULL, "open",, NULL, NULL, SW_SHOWNORMAL);
+        }
         Sleep(100);
-        *static_cast<uint32_t*>(shm_data) = GetTickCount();
     };
 }
