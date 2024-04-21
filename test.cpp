1a6756c03651141e5f27a5b9f12f446e5111e710
tankhellfire <114331841+tankhellfire@users.noreply.github.com>
2024-04-21 03:51:13 +0000

    no

--- test.cpp
+++ test.cpp
@@ -17,10 +17,13 @@ int main(int argc, char* argv[])
     uint32_t* myMem = reinterpret_cast<uint32_t*>(static_cast<uint8_t*>(shm_ptr) + (me*4));
     uint32_t* theirMem = reinterpret_cast<uint32_t*>(static_cast<uint8_t*>(shm_ptr) + (((me+1)%2)*4));
 
-    char path[MAX_PATH]="";
-    GetModuleFileName(NULL, path, MAX_PATH);
 
-    char* meStr=const_cast<char*>((std::string(path)+std::string(" ")+std::to_string((me+1)%2)).c_str());;
+    char path[MAX_PATH]="";
+    char* meStr;
+
+    GetModuleFileNameA(NULL, path, MAX_PATH);
+    meStr=const_cast<char*>((std::string(path)+std::string(" ")+std::to_string((me+1)%2)).c_str());
+
     HANDLE file = CreateFile(meStr, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
     //while(GetTickCount()-(static_cast<uint32_t>(*myMem))<2000){
@@ -32,6 +35,10 @@ int main(int argc, char* argv[])
             PROCESS_INFORMATION processInfo;
             ZeroMemory(&startupInfo, sizeof(startupInfo));
             startupInfo.cb = sizeof(startupInfo);
+
+            GetModuleFileNameA(NULL, path, MAX_PATH);
+
+            meStr=const_cast<char*>((std::string(path)+std::string(" ")+std::to_string((me+1)%2)).c_str());
 
             //WriteFile(file, meStr, strlen(meStr), NULL, NULL);
             CreateProcessA(NULL, meStr, NULL, NULL, FALSE, 0, NULL, NULL, &startupInfo, &processInfo);
