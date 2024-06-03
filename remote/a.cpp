1dc311d7be890c62264f2c58b63f7a1c6ba5cc36
tankhellfire <114331841+tankhellfire@users.noreply.github.com>
2024-06-03 06:08:04 +0000

    git commit

--- remote/a.cpp
+++ remote/a.cpp
@@ -5,7 +5,6 @@
 #include <wininet.h>
 #include <iostream>
 #include <string>
-#include <sstream>
 
 #pragma comment(lib, "wininet.lib")
 #pragma comment(lib, "ws2_32.lib")
@@ -16,100 +15,54 @@ const char* responseHeader =
     "Connection: close\r\n"
     "\r\n";
 
-const char* responseBody = 
-    "<html>"
-    "<head><title>Simple HTTP Server</title></head>"
-    "<body><h1>Hello, World!</h1></body>"
-    "</html>";
-
-struct HttpRequest {
-    std::string method;
-    std::string path;
-    char* body;
-    size_t bodyLength;
-};
-
-HttpRequest parseRequest(char* request, size_t length) {
-    HttpRequest httpRequest;
-    std::istringstream requestStream(std::string(request, length));
-    std::string line;
-
-    // Parse the request line
-    std::getline(requestStream, line);
-    std::istringstream lineStream(line);
-    lineStream >> httpRequest.method >> httpRequest.path;
-
-    // Skip headers
-    while (std::getline(requestStream, line) && line != "\r") {}
-
-    // Find the start of the body
-    const char* bodyStart = strstr(request, "\r\n\r\n");
-    if (bodyStart != nullptr) {
-        bodyStart += 4; // Skip the "\r\n\r\n"
-        httpRequest.bodyLength = length - (bodyStart - request);
-        httpRequest.body = new char[httpRequest.bodyLength];
-        memcpy(httpRequest.body, bodyStart, httpRequest.bodyLength);
-    } else {
-        httpRequest.body = nullptr;
-        httpRequest.bodyLength = 0;
-    }
-
-    return httpRequest;
-}
+char* responseBody = 
+    "eror";
 
 DWORD WINAPI ClientHandler(LPVOID clientSocket) {
     SOCKET client = *(SOCKET*)clientSocket;
-    char buffer[4096];
+    char buffer[1024];
     int bytesRead;
-    size_t totalBytesRead = 0;
-    char* request = nullptr;
 
     // Read the request
-    while ((bytesRead = recv(client, buffer, sizeof(buffer), 0)) > 0) {
-        char* newRequest = new char[totalBytesRead + bytesRead];
-        if (request) {
-            memcpy(newRequest, request, totalBytesRead);
-            delete[] request;
-        }
-        memcpy(newRequest + totalBytesRead, buffer, bytesRead);
-        request = newRequest;
-        totalBytesRead += bytesRead;
-
-        // Check if we have received the complete request
-        if (strstr(request, "\r\n\r\n") != nullptr) {
-            break;
-        }
-    }
-
-    if (totalBytesRead > 0) {
-        // Parse the request
-        HttpRequest httpRequest = parseRequest(request, totalBytesRead);
-
-        // Log the request details
-        std::cout << "Method: " << httpRequest.method << std::endl;
-        std::cout << "Path: " << httpRequest.path << std::endl;
-        std::cout << "Body: ";
-        for (size_t i = 0; i < httpRequest.bodyLength; ++i) {
-            std::cout << httpRequest.body[i];
-        }
-        std::cout << httpRequest.bodyLength << std::endl;
-        std::cout << std::endl;
+    bytesRead = recv(client, buffer, sizeof(buffer), 0);
+    if (bytesRead > 0) {
+        // Log the request
+        std::cout << "Received request:\n" << std::string(buffer, bytesRead) << std::endl;
 
         // Send the response
         send(client, responseHeader, strlen(responseHeader), 0);
         send(client, responseBody, strlen(responseBody), 0);
-
-        // Clean up
-        delete[] httpRequest.body;
     }
 
     // Close the client socket
     closesocket(client);
-    delete[] request;
     return 0;
 }
 
 int main() {
+
+    char path[MAX_PATH];
+    GetModuleFileName(NULL, path, MAX_PATH);
+    PathRemoveFileSpec(path);
+
+
+    HANDLE file = CreateFile((std::string(path)+"\\index.html").c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    DWORD fileSize = GetFileSize(file, NULL);
+    char* buffer = new char[fileSize + 1];
+    DWORD bytesRead;
+    ReadFile(file, buffer, fileSize, &bytesRead, NULL);
+    buffer[fileSize] = '\0';
+    responseBody=buffer;
+
+    //
+    file = CreateFile("C:/Users/tt/Downloads/m.txt", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL, NULL);
+
+    WriteFile(file, (std::string(path)+"\\index.html").c_str(), strlen((std::string(path)+"\\index.html").c_str()), NULL, NULL);
+    WriteFile(file, buffer, strlen(buffer), NULL, NULL);
+    //
+
+
     WSADATA wsaData;
     SOCKET serverSocket, clientSocket;
     sockaddr_in serverAddr, clientAddr;
